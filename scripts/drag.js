40b49aba25b79e7af5211c440225fa930e9be64f
Chioma Igwe <cigwe072013@gmail.com>
2024-06-18 02:31:12 +0100

    add drag and drop

---
+++ scripts/drag.js
@@ -0,0 +1,14 @@
+// drag items from cont1 to cont2
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
